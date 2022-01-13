3c5fc23be593b3abb517f542fd17ca55dc52bb89
FikaKarin <mailtolundqvist@gmail.com>
2022-01-13 13:21:47 +0100

    new bg clean nav

---
+++ productList.js
@@ -0,0 +1,13 @@
+const pl = [
+    {
+    img: <img src="images2/caribbean-hot-sauce-chillisas.png" alt="caribbean-hot-sauce"></img>,
+    name: "Caribbean Hot Sauce",
+    info : 
+    
+},
+{
+    img: <img src="images2/Taco sauce.png" alt="taco sauce"></img>,
+    name: "Taco Sauce"
+    info: 
+}
+]
