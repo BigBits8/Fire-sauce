1991a5d8378e5b898311ffa1f4183d4b961f0012
FikaKarin <mailtolundqvist@gmail.com>
2022-01-28 13:24:56 +0100

    deleteknapp fixad

--- cart.js
+++ cart.js
@@ -10,35 +10,31 @@ function badgeItem() {
 function addToCart(index){
     let cart = ``;
     cartItems.push(db[index]);
+    
     cartItems.forEach(element => {
+        let elementIndex = cartItems.indexOf(element)
         cart += `
-        <tr>
+        <tr id='cart-item-${elementIndex}'>
             <td>${element.name}</td>
             <td>${element.price}</td>
-            <td><button type='button' class='btn-remove' data-id='${index}'>Delete</button></>
+            <td><button onClick='deleteFromCart(${index}, ${elementIndex})' type='button' class='btn-remove' data-id='${index}'>Delete</button></>
         </tr>
         `
     }); 
-    cartTableBody.innerHTML = cart;  
-    badgeItem();
-    // DELETE BUTTON
-    const buttonsRemove = document.querySelectorAll(`.btn-remove`);
-    console.log(buttonsRemove);
 
-    for (let i = 0; i < buttonsRemove.length; i++) {
-    const button = buttonsRemove[i];
-    button.addEventListener('click', function(event){
-        // console.log('button clicked');
-        const buttonClicked = event.target;
-        console.log(buttonClicked);
-        buttonClicked.parentElement.parentElement.remove();
-        cartItems.splice(i, 1);
-        console.log(cartItems);
-        badgeItem()
-    })
-   }
-   // DELETE BUTTON SLUT
+    cartTableBody.innerHTML = cart;
+    badgeItem();
    
+}
+
+function deleteFromCart(index, elementIndex){
+    cartItems.pop(db[index]); // Ta bort från korgen
+
+    saveCart(); // Spara korgen i localstorage
+    badgeItem(); // Uppdatera siffran
+
+    var rowToRemove = document.getElementById(`cart-item-${elementIndex}`); // Hitta raden vi vill ta bort (HTML)
+    rowToRemove.remove(); // Ta bort raden
 }
 
 //SAVES cartItem TO CART ARRAY 
